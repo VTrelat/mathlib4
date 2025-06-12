0acb105276099f0bb0b22aa74c7565c9d4aaa21e
VTrelat <vincent.trelat@depinfonancy.net>
2025-06-12 09:38:04 +0200

    doc: fix typo

--- Mathlib/SetTheory/ZFC/Naturals.lean
+++ Mathlib/SetTheory/ZFC/Naturals.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2025 Vicnent Trélat. All rights reserved.
+Copyright (c) 2025 Vincent Trélat. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Vicnent Trélat
 -/
